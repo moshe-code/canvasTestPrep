98ff072e85a4089142e2b6c4b74ae59408906c1f
Moshe Weiner <moshesweiner@gmail.com>
2022-01-27 17:15:31 +0200

    finish mouse coord

--- canvastest/index.js
+++ canvastest/index.js
@@ -1,27 +1,36 @@
-// let myCanvas = document.getElementById("myCanvas")
-// let ctx = myCanvas.getContext("2d")
-// ctx.fillRect(100,100,100,100)
+let myCanvas = document.getElementById("myCanvas")
+let ctx = myCanvas.getContext("2d")
+ctx.fillRect(100,100,100,100)
 
 
-// ctx.strokeRect(100,300,100,100)
+ctx.strokeRect(100,300,100,100)
 
-window.onload = function (){
-    let myCanvas = document.getElementById("myCanvas")
-    let ctx = myCanvas.getContext("2d")
+// window.onload = function (){
+//     let myCanvas = document.getElementById("myCanvas")
+//     let ctx = myCanvas.getContext("2d")
 
-    myCanvas.addEventListener("mousemove", function(e){
-        let x = e.clientX, y = e.clientY
+//     myCanvas.addEventListener("mousemove", function(e){
+//         let x = e.clientX, y = e.clientY
 
-    })
-}
+//     })
+// }
 
 function showCoords(event) {
     let x = event.clientX;
     let y = event.clientY;
-    let coords = "X coords: " + x + ", Y coords: " + y;
-    document.getElementById("mouseLocation").innerHTML = coords;
-    // console.log(coords);
+    let coords = "X: " + x + ", Y: " + y;
+    document.getElementById("mouseLocation").innerText = coords;
+    console.log(coords);
   }
   function clearCoor() {
     document.getElementById("clear").innerHTML = "";
   }
+
+  
+  
+    document.getElementById("draw-button").addEventListener("click",() =>{
+      for (i =0; i < 3; i++)
+      alert("OOOPS!! You clicked out of range...")
+
+
+    })
